fbebde5468eeee1b5a43872b06c0d5dbecf1319e
LahceneDer <l.dergham@esi-sba.dz>
2021-12-26 00:23:56 +0100

    remove unused things

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './App.css';
-import ToursApp from './components/tours/ToursApp';
 
 
 
